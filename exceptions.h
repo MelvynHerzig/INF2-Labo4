2127c099268c9f8f41800c9cb8121966542d7307
MelvynHerzig <34660483+MelvynHerzig@users.noreply.github.com>
2020-04-20 14:25:10 +0200

    Ajout exceptions

--- exceptions.h
+++ exceptions.h
@@ -1,13 +1,16 @@
 /*
  -----------------------------------------------------------------------------------
- Laboratoire : <nn>
- Fichier     : Labo_XX_Crausaz.cpp
- Auteur(s)   : Nicolas Crausaz
- Date        : <jj.mm.aaaa>
+ Laboratoire : 04
+ Fichier     : exceptions.h
+ Auteur(s)   : Nicolas Crausaz, Forestier Quentin, Herzig Melvyn
+ Date        : 20.04.2020
 
- But         : <à compléter>
+ But         : Implémenter deux classes exceptions pour:
 
- Remarque(s) : <à compléter>
+               -  La manipulation de prix.
+               -  L'utilisation d'incides.
+
+ Remarque(s) : Les deux classes héritent de std::logic_error
 
  Compilateur : MinGW-g++ 6.3.0
  -----------------------------------------------------------------------------------
@@ -15,5 +18,22 @@
 
 #ifndef INF2_LABO4_EXCEPTIONS_H
 #define INF2_LABO4_EXCEPTIONS_H
+
+#include <stdexcept>
+#include <string>
+
+class IndiceNonValide : public std::logic_error
+{
+public:
+   explicit IndiceNonValide(const std::string& s) : logic_error(s) {};
+   explicit IndiceNonValide(const char* s)        : logic_error(s) {};
+};
+
+class PrixNonValide : public std::logic_error
+{
+public:
+   explicit PrixNonValide(const std::string& s) : logic_error(s) {};
+   explicit PrixNonValide(const char* s)        : logic_error(s) {};
+};
 
 #endif //INF2_LABO4_EXCEPTIONS_H
